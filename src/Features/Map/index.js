97d52ed1e2ce9ec5023d3484cd8ccee3d1305d76
Jake <jacobartz1987@gmail.com>
2019-05-14 22:45:40 -0600

    Player and Map and World components

---
+++ src/Features/Map/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Map(props) {
+    return (
+        <div
+          style={{
+              width : '800px',
+              height : '400px',
+              backgroundColor: 'green',
+              margin: 'auto',
+              marginTop: '10%',
+
+          }}
+        />
+    )
+}
+
+export default Map
---
+++ src/Features/World/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Player from '../Player'
+import Map from '../Map'
+
+function World(props) {
+    return (
+        <div
+            style={{
+                position : 'relative',
+                margin : '10% auto',
+                width : '800px',
+                height : '400px',
+
+
+            }}
+        >
+            <Player />
+            <Map />
+            
+        </div>
+    )
+}
+
+export default World
