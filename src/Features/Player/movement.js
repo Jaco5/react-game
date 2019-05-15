d25d4bc99899d3c642e09cf6912ccdd00db5ac48
Jake <jacobartz1987@gmail.com>
2019-05-15 12:57:29 -0600

    boundary handling

--- src/Features/Player/movement.js
+++ src/Features/Player/movement.js
@@ -1,5 +1,5 @@
 import store from '../../config/store'
-import { SPRITE_SIZE } from '../../config/constants'
+import { SPRITE_SIZE, MAP_WIDTH, MAP_HEIGHT } from '../../config/constants'
 
 export default function handleMovement(player) {
 
@@ -7,25 +7,31 @@ export default function handleMovement(player) {
         const oldPos = store.getState().player.position
         switch (direction) {
             case 'WEST':
-                return [oldPos[0]-SPRITE_SIZE, oldPos[1]]
+                return [oldPos[0] - SPRITE_SIZE, oldPos[1]]
             case 'NORTH':
-                return [oldPos[0], oldPos[1]-SPRITE_SIZE]
+                return [oldPos[0], oldPos[1] - SPRITE_SIZE]
             case 'EAST':
-                return [oldPos[0]+SPRITE_SIZE, oldPos[1]]
+                return [oldPos[0] + SPRITE_SIZE, oldPos[1]]
             case 'SOUTH':
-                 return [oldPos[0], oldPos[1]+SPRITE_SIZE]
+                return [oldPos[0], oldPos[1] + SPRITE_SIZE]
             default:
                 return console.log("getNewPosition default case")
         }
+    }
 
+    function observeBoundaries(oldPos, newPos) {
+        return (newPos[0] >= 0 && newPos[0] <= MAP_WIDTH) &&
+            (newPos[1] >= 0 && newPos[1] <= MAP_HEIGHT)
+            ? newPos : oldPos
     }
 
     function dispatchMove(direction) {
 
+        const oldPos= store.getState().player.position
         store.dispatch({
             type: 'MOVE_PLAYER',
             payload: {
-                position: getNewPosition(direction)
+                position: observeBoundaries(oldPos, getNewPosition(direction))
 
             }
         })
